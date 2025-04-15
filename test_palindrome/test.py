4a0bcb8052ce8c40c9fd4f109248418cde56ee31
Bsturniolo <b.sturniolo@alumno.um.edu.ar>
2025-04-15 19:50:38 -0300

    test palindrome

--- test_palindrome/test.py
+++ test_palindrome/test.py
@@ -6,6 +6,11 @@ class TestPalindrome(unittest.TestCase):
         self.assertEqual(is_palindrome("Neuquen"), True)
         self.assertEqual(is_palindrome("Radar"), True)
         self.assertEqual(is_palindrome("Ana"), True)
+        
+    def test_palindrome2(self):
+        self.assertEqual(is_palindrome("Eve"), True)
+        self.assertEqual(is_palindrome("Reviver"), True)
+        self.assertEqual(is_palindrome("Ojo rojo"), True)
 
 if __name__ == "__main__":
     unittest.main()
